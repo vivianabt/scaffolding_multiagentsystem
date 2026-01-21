416a8cd4526b0b7acce41d39e3ce61bcb4dad38f
vivianabt <vivian@abt-net.de>
2026-01-21 18:54:20 +0100

    Update streamlit_experimental_session.py

--- MAS/app/streamlit_experimental_session.py
+++ MAS/app/streamlit_experimental_session.py
@@ -972,7 +972,7 @@ class StreamlitExperimentalSession:
                     education_level != "Please select...",
                     activity,
                     background != "Please select...",
-                    confidencechat != "Please select..."
+                    confidencechat
                 ]):
                     st.error("Bitte füllen Sie alle Pflichtfelder aus.")
                     return None
