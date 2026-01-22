9fa588d7220a7cad9089b6f27c504f6d987b3503
vivianabt <vivian@abt-net.de>
2026-01-22 13:45:32 +0100

    Update CTL streamlit_experimental_session.py

--- MAS/app/streamlit_experimental_session.py
+++ MAS/app/streamlit_experimental_session.py
@@ -222,7 +222,7 @@ class StreamlitExperimentalSession:
             {
                 "construct": "GCL",
                 "code": "GCL3",
-                "statement": "Die Lerneinheit enthielt Elemente, die mich unterstützten, den Lernstoff besser zu verstehen."
+                "statement": "Die Lerneinheit enthielt Inhalte, die mich unterstützten, den Lernstoff besser zu verstehen."
             },
             # {
             #     "construct": "GCL",
